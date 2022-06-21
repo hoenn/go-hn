e5681414b4784e7420a08c294ed603e33e22af08
hoenn <hoenn@github.com>
2022-06-21 12:05:55 -0400

    Use new api in cli

--- cmd/go-hn-cli/cmd/item.go
+++ cmd/go-hn-cli/cmd/item.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/hoenn/go-hn/pkg/hnapi"
 	"github.com/spf13/cobra"
 )
 
@@ -22,10 +23,37 @@ var getItemCmd = &cobra.Command{
 	Short: "Get an item by ID from the hackernews api",
 	Long:  "Gets an item (Story, Comment, Poll, PollOpt) from the hacker news api by its item ID",
 	Run: func(cmd *cobra.Command, args []string) {
-		item, err := client.Item(itemID)
+		item, err := client.GetItem(itemID)
 		if err != nil {
 			errorMsgWithExit(err)
 		}
-		fmt.Print(prettyPrint(item))
+		switch item.Type {
+		case hnapi.StoryItem, hnapi.JobItem, hnapi.AskItem:
+			s, err := hnapi.ItemToStory(item)
+			if err != nil {
+				errorMsgWithExit(fmt.Errorf("could not read story item: %w", err))
+			}
+			fmt.Print(prettyPrint(s))
+		case hnapi.CommentItem:
+			c, err := hnapi.ItemToComment(item)
+			if err != nil {
+				errorMsgWithExit(fmt.Errorf("could not read comment item: %w", err))
+			}
+			fmt.Print(prettyPrint(c))
+		case hnapi.PollItem:
+			p, err := hnapi.ItemToPoll(item)
+			if err != nil {
+				errorMsgWithExit(fmt.Errorf("could not read poll item: %w", err))
+			}
+			fmt.Print(prettyPrint(p))
+		case hnapi.PollOptItem:
+			p, err := hnapi.ItemToPollOpt(item)
+			if err != nil {
+				errorMsgWithExit(fmt.Errorf("could not read pollopt item: %w", err))
+			}
+			fmt.Print(prettyPrint(p))
+		default:
+			fmt.Println(prettyPrint(item))
+		}
 	},
 }
