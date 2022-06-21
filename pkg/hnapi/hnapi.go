6b0a13352dd30c5951c48d705ab1e649004b73f5
hoenn <hoenn@github.com>
2022-06-21 11:49:16 -0400

    Add Item type and helper functions, add deprecation notice to Item func

--- pkg/hnapi/hnapi.go
+++ pkg/hnapi/hnapi.go
@@ -33,6 +33,9 @@ func NewHNClientWithURL(url string) *HNClient {
 	}
 }
 
+// Deprecated: Item is deprecated. Use GetItem or a more specialized method like GetComment instead.
+// This was deprecated because it pushed type casting into client code. There are now helper like
+// GetStory and ItemToComment to avoid returning an interface{} for clients to deal with.
 // Item issues a GET request on the /item/<id> path and creates a struct
 // from the response body.
 // It returns an empty interface to be asserted into one of the hnapi types
@@ -52,41 +55,184 @@ func (h *HNClient) Item(id string) (interface{}, error) {
 		return nil, fmt.Errorf("could not read response body: %w", err)
 	}
 
-	var t msgWrapper
-	if err := json.Unmarshal(body, &t); err != nil {
+	var i Item
+	if err := json.Unmarshal(body, &i); err != nil {
 		return nil, fmt.Errorf("could not unmarshal response body: %w", err)
 	}
 	var res interface{}
-	switch t.Type {
-	case "story", "job", "ask":
+	switch i.Type {
+	case StoryItem, JobItem, AskItem:
 		s := &Story{}
 		if err := json.Unmarshal(body, s); err != nil {
 			return nil, err
 		}
 		res = s
-	case "comment":
+	case CommentItem:
 		c := &Comment{}
 		if err := json.Unmarshal(body, c); err != nil {
 			return nil, err
 		}
 		res = c
-	case "poll":
+	case PollItem:
 		p := &Poll{}
 		if err := json.Unmarshal(body, p); err != nil {
 			return nil, err
 		}
 		res = p
-	case "pollopt":
+	case PollOptItem:
 		p := &PollOpt{}
 		if err := json.Unmarshal(body, p); err != nil {
 			return nil, err
 		}
 		res = p
 	default:
-		return nil, fmt.Errorf("unknown item type, open a github issue: %w", err)
+		return nil, fmt.Errorf("unknown item type: '%s'", i.Type)
 	}
 
 	return res, nil
+}
+
+const (
+	CommentItem = "comment"
+	PollItem    = "poll"
+	PollOptItem = "pollopt"
+	JobItem     = "job"
+	AskItem     = "ask"
+	StoryItem   = "story"
+)
+
+var storyItemTypes = map[string]bool{
+	JobItem:   true,
+	AskItem:   true,
+	StoryItem: true,
+}
+
+// GetItem returns an *Item from the API.
+func (h *HNClient) GetItem(id string) (*Item, error) {
+	url := h.objURLString("item", id)
+	resp, err := h.httpClient.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("could not make GET request for item: %w", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("non 200 response code: %w", err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("could not read response body: %w", err)
+	}
+
+	i := &Item{}
+	if err := json.Unmarshal(body, i); err != nil {
+		return nil, fmt.Errorf("could not unmarshal response body: %w", err)
+	}
+	return i, nil
+}
+
+// ItemToComment converts an Item into a Comment.
+func ItemToComment(item *Item) (*Comment, error) {
+	if item.Type != CommentItem {
+		return nil, fmt.Errorf("item is not a comment")
+	}
+	return &Comment{
+		By:     item.By,
+		ID:     item.ID,
+		Kids:   item.Kids,
+		Parent: item.Parent,
+		Text:   item.Text,
+		Time:   item.Time,
+		Type:   item.Type,
+	}, nil
+}
+
+// GetComment is a helper function that gets an Item and converts it to a Comment or errors.
+func (h *HNClient) GetComment(id string) (*Comment, error) {
+	item, err := h.GetItem(id)
+	if err != nil {
+		return nil, fmt.Errorf("could not get comment item: %w", err)
+	}
+	return ItemToComment(item)
+}
+
+// ItemToStory converts an Item into a Story.
+func ItemToStory(item *Item) (*Story, error) {
+	if _, ok := storyItemTypes[item.Type]; !ok {
+		return nil, fmt.Errorf("item is not a story")
+	}
+	return &Story{
+		By:          item.By,
+		Descendants: item.Descendants,
+		ID:          item.ID,
+		Kids:        item.Kids,
+		Score:       item.Score,
+		Time:        item.Time,
+		Title:       item.Title,
+		Type:        item.Type,
+		URL:         item.URL,
+	}, nil
+}
+
+// GetStory is a helper function that gets an Item and converts it to a Story or errors.
+func (h *HNClient) GetStory(id string) (*Story, error) {
+	item, err := h.GetItem(id)
+	if err != nil {
+		return nil, fmt.Errorf("could not get story item: %w", err)
+	}
+	return ItemToStory(item)
+}
+
+// ItemToPoll converts an Item into a Poll.
+func ItemToPoll(item *Item) (*Poll, error) {
+	if item.Type != PollItem {
+		return nil, fmt.Errorf("item is not a poll")
+	}
+	return &Poll{
+		By:          item.By,
+		Descendants: item.Descendants,
+		ID:          item.ID,
+		Kids:        item.Kids,
+		Parts:       item.Parts,
+		Score:       item.Score,
+		Text:        item.Text,
+		Time:        item.Time,
+		Title:       item.Title,
+		Type:        item.Type,
+	}, nil
+}
+
+// GetPoll is a helper function that gets an Item and converts it to a Poll or errors.
+func (h *HNClient) GetPoll(id string) (*Poll, error) {
+	item, err := h.GetItem(id)
+	if err != nil {
+		return nil, fmt.Errorf("could not get poll item: %w", err)
+	}
+	return ItemToPoll(item)
+}
+
+// ItemToPollOpt converts an Item into a PollOpt.
+func ItemToPollOpt(item *Item) (*PollOpt, error) {
+	if item.Type != PollOptItem {
+		return nil, fmt.Errorf("item is not a pollopt")
+	}
+	return &PollOpt{
+		By:    item.By,
+		ID:    item.ID,
+		Poll:  item.Poll,
+		Score: item.Score,
+		Text:  item.Text,
+		Time:  item.Time,
+		Type:  item.Type,
+	}, nil
+}
+
+// GetPollOpt is a helper function that gets an Item and converts it to a Pollopt or errors.
+func (h *HNClient) GetPollOpt(id string) (*PollOpt, error) {
+	item, err := h.GetItem(id)
+	if err != nil {
+		return nil, fmt.Errorf("could not get pollopt item: %w", err)
+	}
+	return ItemToPollOpt(item)
 }
 
 // User issues a GET request on the /user/<id> path and
@@ -239,6 +385,26 @@ type HNUser struct {
 
 type msgWrapper struct {
 	Type string
+}
+
+// Item represents a HackerNews API item. Its properties are a superset of
+// Story, Comment, Poll, and Pollopt types.
+type Item struct {
+	ID          int
+	Deleted     bool
+	Type        string
+	By          string
+	Time        int64
+	Text        string
+	Dead        bool
+	Parent      int
+	Poll        int
+	Kids        []int
+	URL         string
+	Score       int
+	Title       string
+	Parts       []int
+	Descendants int
 }
 
 // Comment represents a HackerNews comment on a story.
